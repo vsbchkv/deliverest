b40150ae5919251f56403c7487a045457b637861
Marina Brianova <marina.brianova@gmail.com>
2021-12-13 02:11:14 +0300

    add modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
-// js here
+$(document).ready(function () {
+  $('.js-open-modal').on('click', function (e) {
+    $('.layout').addClass('show');
+    $('body').addClass('body--fixed');
+  });
+
+  function modalClose() {
+    $('.layout').removeClass('show');
+    $('body').removeClass('body--fixed');
+  }
+
+  $('.layout, .modal__close').click(function (e) {
+    if(!$(e.target).closest('.modal').length && !$(e.target).is('.modal')) {
+      modalClose();
+    }
+  });
+  $('.modal__close').click(function (e) {
+    modalClose();
+  });
+
+  $('textarea.text-field').on('focus blur', function (e) {
+    $(this).toggleClass('text-field--expanded');
+  });
+});
