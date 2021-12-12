7450766326d73c07a0179c008e865934ea35660a
Marina Brianova <marina.brianova@gmail.com>
2021-12-13 02:50:42 +0300

    add header locales

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,32 @@
 $(document).ready(function () {
   $('.js-open-modal').on('click', function (e) {
-    $('.layout').addClass('show');
+    $('.layout').addClass('layout--show');
     $('body').addClass('body--fixed');
   });
 
   function modalClose() {
-    $('.layout').removeClass('show');
+    $('.layout').removeClass('layout--show');
     $('body').removeClass('body--fixed');
   }
 
-  $('.layout, .modal__close').click(function (e) {
+  $('.layout').click(function (e) {
     if(!$(e.target).closest('.modal').length && !$(e.target).is('.modal')) {
       modalClose();
     }
   });
   $('.modal__close').click(function (e) {
     modalClose();
+  });
+
+  $('.locale-dropdown').on('click', function (e) {
+    $(this).toggleClass('locales__item--active').find('.locale-dropdown__list').toggleClass('locale-dropdown__list--expanded');
+  });
+
+  $('body').click(function (e) {
+    if(!$(e.target).closest('.locale-dropdown').length && !$(e.target).is('.locale-dropdown')) {
+      $('.locale-dropdown').removeClass('locales__item--active')
+      $('.locale-dropdown__list').removeClass('locale-dropdown__list--expanded');
+    }
   });
 
   $('textarea.text-field').on('focus blur', function (e) {
