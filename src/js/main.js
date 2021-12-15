70e4040227475c8a329cf03f51e10b16a67050e1
Marina Brianova <marina.brianova@gmail.com>
2021-12-15 22:45:21 +0300

    add slider

--- src/js/main.js
+++ src/js/main.js
@@ -32,4 +32,55 @@ $(document).ready(function () {
   $('textarea.text-field').on('focus blur', function (e) {
     $(this).toggleClass('text-field--expanded');
   });
+
+  /*var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 0,
+    centeredSlides: true,
+    //slidesPerGroup: 3,
+    loop: true,
+    //loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });*/
+
+  $('.your-class').slick({
+    centerMode: true,
+    variableWidth: true,
+    //customPaging: '60px',
+    //centerPadding: '60px',
+    slidesToShow: 3,
+    focusOnSelect: true,
+    //arrows: true,
+    appendArrows: $('.custom-slider__nav'),
+    dots: true,
+    dotsClass: 'slick-dots',
+    appendDots: $('.custom-slider__nav'),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          //dots: false
+        }
+      }
+    ]
+  });
 });
+
+/*document.addEventListener( 'DOMContentLoaded', function() {
+  var splide = new Splide( '.splide', {
+    type     : 'loop',
+    width   : '30vw',
+    focus    : 'center',
+    autoWidth: true,
+  } );
+
+  splide.mount();
+} );*/
